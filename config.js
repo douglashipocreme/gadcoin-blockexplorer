97ac39ec46564cb83910a604458cdd3d12988065
Douglas Hipocreme <douglas@hipocreme.com.br>
2018-04-11 11:41:54 -0300

    gadcoin-blockexplorer commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var api = 'https://blockchain.gadcoin.com.br/blockapi';
+var blockTargetInterval = 120;
+var coinUnits = 1000000000;
+var symbol = 'GAD';
+var refreshDelay = 30000;
+// pools stats by MainCoins
+var networkStat = {
+    "gad": [
+		["https://api.gadcoin.com.br","pool.gadcoin.com.br", "gad"]
+		
+    ]
+};
